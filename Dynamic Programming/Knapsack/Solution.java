2c6bd3dfa82bda9c9eaa95065918ddfab99fa99f
Joon88 <joon.oracle@gmail.com>
2024-11-16 20:54:18 +0530

    update

--- Dynamic Programming/Knapsack/Solution.java
+++ Dynamic Programming/Knapsack/Solution.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Solution {
-    int[][] memo;
+    static int[][] memo;
     public static void main(String[] args){
         int[] weights = new int[]{};
         int[] profits = new int[]{};
@@ -16,7 +16,7 @@ public class Solution {
         System.out.println(maxWeight(weights.length, capacity, weights, profits));
     }
     // (Exhaustive method) O(2^n) time and O(n) space for the recursion stack, where n is size of weights array
-    public static int maxWeight(int n int cap, int[] weights, int[] profits) {
+    public static int maxWeight(int n, int cap, int[] weights, int[] profits) {
         if(cap == 0 || n == 0)
             return 0;
         if(cap < weights[n-1])
@@ -24,7 +24,7 @@ public class Solution {
         return Math.max(maxWeight(n-1, cap, weights, profits), profits[n-1] + maxWeight(n-1, cap-weights[n-1], weights, profits));
     }
     // O(nc) time and O(nc) space, where n is size of weights array and c is the capcity of the knapsack
-    public static int maxWeight(int n int cap, int[] weights, int[] profits) {
+    public static int maxWeight1(int n, int cap, int[] weights, int[] profits) {
         if(n == 0 || cap == 0)
             return 0;
         if(memo[n][cap] >= 0)
@@ -37,7 +37,7 @@ public class Solution {
     }
     // 2-D DP
     // O(nw) Time and space : n & w are number of weights avaiable and capacity of knapsack respectively
-    public static int maxWeight(int n, int cap, int[] weights, int[] profits) {
+    public static int maxWeight2(int n, int cap, int[] weights, int[] profits) {
         int[][] dp = new int[n+1][cap+1];
         // by default all array elts will be 0, but mentioned this just to be aware of the base case
         for(int i = 0; i < dp.length ; i++) {
@@ -57,7 +57,7 @@ public class Solution {
 
     // 2*1-D DP
     // O(nw) Time and O(2*w) space : n & w are number of weights avaiable and capacity of knapsack respectively
-    public static int maxWeight(int n, int cap, int[] weights, int[] profits) {
+    public static int maxWeight3(int n, int cap, int[] weights, int[] profits) {
         int[][] dp = new int[2][cap+1];
         // by default all array elts will be 0, but mentioned this just to be aware of the base case
         for(int i = 0; i < dp.length ; i++) {
@@ -79,7 +79,7 @@ public class Solution {
 
     // 1-D DP
     // O(nw) Time and O(w) space : n & w are number of weights avaiable and capacity of knapsack respectively
-    public static int maxWeight(int n, int cap, int[] weights, int[] profits) {
+    public static int maxWeight4(int n, int cap, int[] weights, int[] profits) {
         int[] dp = new int[cap+1];
         for(int i = 0 ; i < weights.length ; i++) {
             for(int wt = cap; wt >= 1; wt--) {
